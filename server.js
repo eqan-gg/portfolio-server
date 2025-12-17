56606f828864b793aaa2a18e89398ec772a8c00d
eqan-gg <eqaanchauhan@gmail.com>
2025-12-17 16:31:49 +0500

    cors origin : link updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://eqan-dev.vercel.app/" }));
 app.use(express.json());
 
 
